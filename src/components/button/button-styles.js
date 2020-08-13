0c37c52bf7ed72bbbfe295f6ed0de7ea2f9e0563
Antônio Sousa <a.sousa.jnri@gmail.com>
2020-08-13 08:08:55 -0300

    Adicionar eslint, prettier

--- src/components/button/button-styles.js
+++ src/components/button/button-styles.js
@@ -1,11 +1,16 @@
-import styled from "styled-components";
+import styled, { css } from 'styled-components';
 
 export const StyledButton = styled.button`
   width: 100%;
   padding: 2rem;
-  background: #fff;
   border-radius: 0.5rem;
   font-size: 1.8rem;
   cursor: pointer;
   transition: 0.3s;
+
+  ${(props) =>
+    props.white &&
+    css`
+      background: #ffffff;
+    `}
 `;
