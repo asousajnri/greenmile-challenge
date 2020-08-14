4c04edf01073af1c7110ccca5fa27dee92bdec30
Antônio Sousa <a.sousa.jnri@gmail.com>
2020-08-14 00:36:04 -0300

    Adicionar position relative no container form.

--- src/components/form/container/container-styles.js
+++ src/components/form/container/container-styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const StyledContainer = styled.form`
+  position: relative;
   padding: 4rem 3rem;
   background: ${props => props.theme.primary_color};
   border-bottom-left-radius: 0.5rem;
